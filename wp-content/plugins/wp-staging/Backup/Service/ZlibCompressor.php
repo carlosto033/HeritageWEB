4165d75a479cb510170f7e81044b3045cc87e4c8
Carlo Stolfa <c.stolfa@studenti.uniba.it>
2025-01-26 18:24:11 +0100

    Inizializzazione del progetto Heritage

---
+++ wp-content/plugins/wp-staging/Backup/Service/ZlibCompressor.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace WPStaging\Backup\Service;
+
+use WPStaging\Backup\Service\Compression\CompressionInterface;
+use WPStaging\Core\WPStaging;
+use WPStaging\Framework\Facades\Hooks;
+use WPStaging\Framework\SiteInfo;
+
+class ZlibCompressor
+{
+    const FILTER_ZLIB_COMPRESSION_ENABLED = 'wpstg.backup.compression.zlib.enabled';
+
+    /** @var CompressionInterface */
+    protected $service;
+
+    /** @var SiteInfo */
+    private $siteInfo;
+
+    public function __construct(CompressionInterface $service)
+    {
+        $this->service  = $service;
+        $this->siteInfo = WPStaging::make(SiteInfo::class);
+    }
+
+    /**
+     * @return bool Whether the server supports compression.
+     */
+    public function supportsCompression(): bool
+    {
+        return function_exists('gzcompress') && function_exists('gzuncompress');
+    }
+
+    /**
+     * @see \WPStaging\Backup\BackupServiceProvider::registerClasses For the filter.
+     * @return bool Whether the user can use compression.
+     */
+    public function canUseCompression(): bool
+    {
+        static $canUseCompression = null;
+
+        if (!is_null($canUseCompression)) {
+            return $canUseCompression;
+        }
+
+        $isPro   = defined('WPSTGPRO_VERSION');
+        $license = get_option('wpstg_license_status');
+
+        $hasActiveLicense = is_object($license) && property_exists($license, 'license') && $license->license === 'valid';
+
+        $canUseCompression = $this->supportsCompression() && $isPro && ($hasActiveLicense || $this->siteInfo->isLocal());
+
+        return $canUseCompression;
+    }
+
+    /**
+     * @return bool True if compression is enabled, false if not.
+     */
+    public function isCompressionEnabled(): bool
+    {
+        static $isEnabled = null;
+
+        // Early bail: if compression feature not enabled.
+        if (!$this->isCompressionFeatureEnabled()) {
+            return false;
+        }
+
+        if (is_null($isEnabled)) {
+            $settings = (object)get_option('wpstg_settings', []);
+            $isEnabled = $settings->enableCompression ?? false;
+        }
+
+        $canUseCompression = $this->canUseCompression();
+
+        return Hooks::applyFilters(self::FILTER_ZLIB_COMPRESSION_ENABLED, $isEnabled && $canUseCompression);
+    }
+
+    public function getService(): CompressionInterface
+    {
+        return $this->service;
+    }
+
+    /** @return bool */
+    protected function isCompressionFeatureEnabled(): bool
+    {
+        $enabled = (bool)Hooks::applyFilters('wpstg.tests.backup.enable_compression', defined('WPSTG_ENABLE_COMPRESSION') && WPSTG_ENABLE_COMPRESSION);
+        return $enabled;
+    }
+}
---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1734344755;s:7:"authKey";s:64:"7$MxNna~tHE -3bvk|{i{{6v:a<G.g9QE498JOPD)U,otuE3bA k?{ *em7Yk7p?";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:1925;}
